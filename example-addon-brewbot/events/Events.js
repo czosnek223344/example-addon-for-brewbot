62a59fe0878c6dbd3828253bc6321d1deb0024fe
4Fix <maybeczosnek223344@gmail.com>
2026-06-05 13:10:45 +0200

    Delete example-addon-brewbot directory

--- example-addon-brewbot/events/Events.js
+++
@@ -1,6 +0,0 @@
-export default function initEvents(i) {
-    /* Example event, you can use every mineflayer event here */
-    i.bot.on('spawn', () => {
-        i.logger.info('Hello, World!');
-    });
-}
